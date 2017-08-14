cbe595196bbd87e76061f4b5be79d60895dab657
Sergio Oller <sergioller@gmail.com>
2017-08-14 12:22:43 +0200

    Simplify build variables

--- src/cmu_us_slt_plugin.c
+++ src/cmu_us_slt_plugin.c
@@ -34,9 +34,6 @@
 /*************************************************************************/
 
 #include "cmu_us_slt.h"
-
-#ifndef ENABLE_CMU_US_SLT_BUILTIN
 #include "cst_plugins.h"
 mimic_plugin_t mimic_plugin = { "voice_cmu_us_slt", 0, &voice_cmu_us_slt_plugin_init, &voice_cmu_us_slt_plugin_exit};
-#endif
 
